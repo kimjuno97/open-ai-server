0fea554aa6a9385669cbaafd17b3d5eb83c275b5
kimjuno97 <jh241093@gmail.com>
2023-03-06 20:25:35 +0900

    cors url 수정

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: 'https://open-ai-git-main-kimjuno97.vercel.app/',
+		origin: 'https://open-ai-git-main-kimjuno97.vercel.app',
 	})
 );
 app.use(morgan('combined'));
